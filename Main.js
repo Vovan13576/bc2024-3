1978e90cb7cbb91b2f5983f34b76b3e0577718e1
Vovan13576 bc2024-3 <vp3098875gmail.com>
2024-10-14 09:14:22 +0300

    покращення коду

--- Main.js
+++ Main.js
@@ -1,27 +1,46 @@
-const {program} = require('commander')
-const fs = require('fs')
+const { program } = require('commander');
+const fs = require('fs');
 
+// Конфігурація командної строки
 program
-.requiredoption('-i, --input <file>')
-.option('-o, --output <file>')
-.option('-d, --display <file>');
+  .requiredOption('-i, --input <file>', 'Input JSON file')
+  .option('-o, --output <file>', 'Output JSON file')
+  .option('-d, --display', 'Display filtered data');
 
-program.parse(process.argv);
+program.parse();
+const options =program.opts();
 
-if (!program.input) {
-    console.error('Please, specify input file');
-    process.exit(1);
-  }
+// Перевірка наявності вхідного файлу
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
 
-  if (!fs.isNull program.input) {
-    console.error('Please, specify input file');
-    process.exit(1);
-  }
+// Читання вхідного JSON-файлу
+const data = fs.readFileSync(options.input, 'utf8');
+let jsonData;
 
-  if (program.display) {
-    console.log(result);
+try {
+  jsonData = JSON.parse(data);
+} catch (err) {
+  console.error('Invalid JSON format');
+  process.exit(1);
+}
+
+// Фільтрація об'єктів з ku=13 та value>5
+const filteredData = jsonData.filter(element => element.ku == 13 && element.value > 5);
+
+// Якщо вказано опцію --output, записуємо відфільтровані дані у файл
+if (options.output) {
+  fs.writeFileSync(options.output, JSON.stringify(filteredData, null, 2), 'utf8');
+  console.log(`Filtered data written to ${options.output}`);
+}
+
+// Якщо вказано опцію --display, виводимо поле txt кожного відфільтрованого елемента
+if (options.display) {
+  if (filteredData.length > 0) {
+    filteredData.forEach(element => console.log(element.txt));
+  } else {
+    console.log('No data found with ku=13 and value > 5');
   }
-  
-  if (program.output) {
-    writeToFile(program.output, result);
-  }
+}
